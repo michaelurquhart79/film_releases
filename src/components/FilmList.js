02f02dfed241e5babc982aa1b37385fd11a0f1f1
Michael Urquhart <michaelurquhart@hotmail.com>
2019-08-12 16:26:45 +0100

    FilmList altered to create filNodes which is an array of FilmItem components. The name and url for each film is passed down to the corresponding FilmItem component.

--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -4,11 +4,15 @@ import FilmItem from './FilmItem';
 class FilmList extends Component{
 
   render() {
+
+    const filmNodes = this.props.data.map(film =>{
+      return <FilmItem key={film.id} name={film.name} url={film.url}/>
+    })
+
     return(
       <div className="film-list">
         <h2>Hi, I'm the film list component</h2>
-        <FilmItem/>
-        <FilmItem/>
+        {filmNodes}
       </div>
     )
   }
