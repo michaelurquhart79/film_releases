a43906536d1cb8e460b6561fd5800ef28eb55b6f
Michael Urquhart <michaelurquhart@hotmail.com>
2019-08-12 16:17:09 +0100

    data set-up in state of FilmBox and passed down to FilmList as a prop. Checked this worked correctly using React dev tools in Chrome.

--- src/containers/FilmBox.js
+++ src/containers/FilmBox.js
@@ -3,11 +3,44 @@ import FilmList from '../components/FilmList'
 
 class FilmBox extends Component{
 
+  constructor(props){
+    super(props);
+    this.state = {
+      data: [
+        {
+          id: 1,
+          name: "Dora and the Lost City of Gold",
+          url: "https://www.imdb.com/title/tt7547410/?ref_=rlm"
+        },
+        {
+          id: 2,
+          name: "Downton Abbey",
+          url: "https://www.imdb.com/title/tt6398184/?ref_=rlm"
+        },
+        {
+          id: 3,
+          name: "Rambo: Last Blood",
+          url: "https://www.imdb.com/title/tt1206885/?ref_=rlm"
+        },
+        {
+          id: 4,
+          name: "Zombieland: Double Tap",
+          url: "https://www.imdb.com/title/tt1560220/?ref_=rlm"
+        },
+        {
+          id: 5,
+          name: "The Addams Family",
+          url: "https://www.imdb.com/title/tt1620981/?ref_=rlm"
+        }
+      ]
+    }
+  }
+
   render() {
     return(
       <div className="film-box">
         <h2>Upcoming Film Releases for UK</h2>
-        <FilmList/>
+        <FilmList data={this.state.data}/>
       </div>
     )
   }
